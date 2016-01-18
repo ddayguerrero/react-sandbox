e013c8a400fafdc61b64ecb0b0b5481ba2e86321
Darrel <ddayy.guerrero@hotmail.com>
2016-01-17 20:42:37 -0500

    Add static index and css

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = ()=> {
+    return <div>Hi!</div>;
+}
+
+// Render
+ReactDOM.render(<App/>, document.querySelector('.container'));
