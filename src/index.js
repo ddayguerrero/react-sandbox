182e7b70d2dc7c081eacc0360843b86eaa4776db
Darrel <ddayy.guerrero@hotmail.com>
2016-01-28 23:37:18 -0500

    Render video detail

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
 import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
 
 // YouTube API Key
 const API_KEY = 'AIzaSyDA-RfWDGFTkvtJcblQaBvM3MdlPQjWdXA';
 
 class App extends Component {
+    // Constructor Function
     constructor(props){
         super(props);
         
@@ -17,10 +19,12 @@ class App extends Component {
             this.setState({ videos:videos });
         });
     }
+    
     render(){
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
                 <VideoList videos={this.state.videos} />
             </div>
         );
