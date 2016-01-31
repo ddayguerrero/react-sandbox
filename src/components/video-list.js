ecac07618529c338209a6bb8054ad34636ddea99
Darrel <ddayy.guerrero@hotmail.com>
2016-01-30 21:51:12 -0500

    Style searchbar

--- src/components/video-list.js
+++ src/components/video-list.js
@@ -4,7 +4,7 @@ import VideoListItem from './video-list-item';
 const VideoList = (props) => {
     const videoItems = props.videos.map((video) => {
        return (
-           <VideoListItem 
+           <VideoListItem
            onVideoSelect={props.onVideoSelect}
            key={video.etag} 
            video={video} /> 
